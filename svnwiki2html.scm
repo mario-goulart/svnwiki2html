4620dbab18f62c67aa176a12df3996bbdb167c0c
Pietro Cerutti <gahr@gahr.ch>
2025-08-20 17:04:11 +0200

    Allow building with C6

--- svnwiki2html.scm
+++ svnwiki2html.scm
@@ -4,7 +4,7 @@
 (cond-expand
  (chicken-4
   (use qwiki utils svnwiki-sxml sxml-transforms))
- (chicken-5
+ ((or chicken-5 chicken-6)
   (import (chicken base)
           (chicken format)
           (chicken pathname)
